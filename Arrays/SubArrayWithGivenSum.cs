732fc9aba2a90c95a0046f654d5cc11ef4391cc5
Vishnu Sudha Vasu Mallipudi (Quadrant Resource LLC) <v-vimall@microsoft.com>
2018-07-19 23:17:00 -0700

    Finding the subarray with the given sum

--- Arrays/SubArrayWithGivenSum.cs
+++ Arrays/SubArrayWithGivenSum.cs
@@ -6,34 +6,24 @@ using System.Threading.Tasks;
 
 namespace Arrays
 {
-    class Program
+    //https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
+    class SubArrayWithGivenSum
     {
-        static void Main(string[] args)
-        {
-            
-            //int ncases = int.Parse(Console.ReadLine());
-            //var result = (Console.ReadLine()).Split(null);
-            int[] a =new int[] { 1, 2, 3, 7 ,5};
-            Console.WriteLine(FindSubArray(12, a));
-            Console.ReadLine();
-
-        }
-
         private static int FindSubArray(int sum, int[] a)
         {
             int currentSum = a[0], start = 0;
             for (int i = 1; i < a.Length; i++)
             {
-                if (currentSum==sum)
+                if (currentSum == sum)
                 {
                     //return from start...i-1
                     return currentSum;
                 }
-                if (currentSum<sum)
+                if (currentSum < sum)
                 {
                     currentSum += a[i];
                 }
-                if (currentSum+a[i]>sum)
+                if (currentSum + a[i] > sum)
                 {
                     currentSum -= a[start];
                     currentSum += a[i];
